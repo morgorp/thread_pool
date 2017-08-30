39594a7cc222bd383c2355eaad89b28dbcaa1c00
morgorp <progrom@foxmail.com>
2017-08-30 16:05:01 +0800

    Add light task

--- benchmask.c
+++ benchmask.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-#define MAX_QUE_SIZE 100000
+#define MAX_QUE_SIZE 1000000
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
@@ -29,9 +29,18 @@ void cpu_tskfunc(void *arg)
 	}
 }
 
+void light_tskfunc(void *arg)
+{
+	double f = 0;
+	for(int i=0; i<10; ++i) {
+		f += i;
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	int thrnum = 1; // 默认线程数 1
+	int light_tsknum = 0; // 默认轻量级任务数 0
 	int cpu_tsknum = 0; // 默认计算任务数 0
 	int io_tsknum = 0; // 默认IO任务数 0
 	
@@ -40,6 +49,9 @@ int main(int argc, char *argv[])
 			switch(argv[i][1]) {
 			case 'n':
 				sscanf(argv[++i], "%d", &thrnum);
+				break;
+			case 'l':
+				sscanf(argv[++i], "%d", &light_tsknum);
 				break;
 			case 'u':
 				sscanf(argv[++i], "%d", &cpu_tsknum);
@@ -58,8 +70,10 @@ int main(int argc, char *argv[])
 	thread_pool_t pool;
 	thread_pool_init(&pool, thrnum, MAX_QUE_SIZE);
 
+	thread_task_t light_tsk;
 	thread_task_t cpu_tsk;
 	thread_task_t io_tsk;
+	thread_task_init(&light_tsk, light_tskfunc, NULL);
 	thread_task_init(&cpu_tsk, cpu_tskfunc, NULL);
 	thread_task_init(&io_tsk, io_tskfunc, NULL);
 
@@ -69,8 +83,10 @@ int main(int argc, char *argv[])
 	for(int i=0; i<cpu_tsknum; ++i) {
 		thread_pool_execute(&pool, &cpu_tsk);
 	}
+	for(int i=0; i<light_tsknum; ++i) {
+		thread_pool_execute(&pool, &light_tsk);
+	}
 
 	thread_pool_shutdown(&pool, 0);
-
 	return 0;
 }
